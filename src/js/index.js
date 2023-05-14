5971e96f24524f1f65b6a5858a859cd68b4f4069
joelbe-thiago <133179296+joelbe-thiago@users.noreply.github.com>
2023-05-14 09:26:44 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,10 +8,10 @@ botaoAlterarTema.addEventListener("click", () => {
     body.classList.toggle("modo-escuro");
 
     if (modoEscuroEstaAtivo) {
-        imagemBotaoTrocaDeTema.setAttribute("src", "/projeto-pokemom/src/imagens/sun.png")
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png")
 
     } else {
-        imagemBotaoTrocaDeTema.setAttribute("src","/projeto-pokemom/src/imagens/moon.png");
+        imagemBotaoTrocaDeTema.setAttribute("src","./src/imagens/moon.png");
 
 
     }
